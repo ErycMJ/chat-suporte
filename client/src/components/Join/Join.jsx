60a14a910ca16d8b6a23d792810da4e044f5ff66
Eryc Jacinto <erycmj@gmail.com>
2024-11-21 21:33:59 -0300

    Teste Vercel

--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -10,7 +10,7 @@ export default function Join({ setChatVisibility, setSocket }) {
     const username = usernameRef.current.value;
     if (!username.trim()) return;
 
-    const socket = await io.connect('http://localhost:3001');
+    const socket = await io.connect('https://chat-suporte-backend.vercel.app/');
     socket.emit('set_username', username);
     setSocket(socket);
     setChatVisibility(true);
